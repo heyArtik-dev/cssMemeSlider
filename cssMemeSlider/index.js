37da64d8aed1eedacef1f1583861ffa29022daaa
heyArtik <heyartik@rambler.ru>
2024-09-01 13:31:45 +0300

    feat: add text animation (Sun, Sep 01, 2024 13:31:24 PM)

--- cssMemeSlider/index.js
+++ cssMemeSlider/index.js
@@ -20,25 +20,27 @@ const images = {
 const meme_image = document.querySelector('.meme-image');
 const text = document.querySelector('.text');
 const pagination = document.querySelector('.pagination');
+const btns = document.querySelectorAll('.btn');
+const classes = meme_image.classList;
+const classesText = text.classList;
 
 meme_image.classList.add(images[0].name);
 text.textContent = images[0].text;
 
-const btns = document.querySelectorAll('.btn');
-// console.log(btns);
-
-const classes = meme_image.classList;
-
-
-
-pagination.addEventListener('click', (e) => {
-  
+pagination.addEventListener('click', (e) => {  
   for (let key in Object.keys(btns)) {
-    
     if (btns[key] === e.target || e.target.closest('.btn') === btns[key]) {
       meme_image.classList.remove(classes[1]);
+      text.classList.add('clear-anim');
+      // text.textContent = '';
       meme_image.classList.add(images[key].name);
-      text.textContent = images[key].text;
+      setTimeout(() => {
+        text.classList.remove('clear-anim'),
+        text.textContent = images[key].text;
+
+      },400) 
+
+      // text.classList.add('anim');
     }
   }
 })
